518a2153b7db1373b6d4b9f33d9323dc2f14eaeb
Twaish <axiomeverything@gmail.com>
2026-05-26 13:10:50 +0200

    Remove .add() from VaultFileStore. Move EncryptedJsonStore read logic to VaultFileStore

--- electron/main/features/vault/adapters/VaultFileStore.ts
+++ electron/main/features/vault/adapters/VaultFileStore.ts
@@ -1,86 +1,18 @@
-import {
-  createReadStream,
-  createWriteStream,
-  readdirSync,
-  ReadStream,
-} from 'fs'
-import { open, readFile, appendFile, unlink, stat } from 'fs/promises'
-import { randomUUID, createDecipheriv } from 'crypto'
-import { pipeline } from 'stream/promises'
-import mime from 'mime-types'
+import { createReadStream, readdirSync, ReadStream } from 'fs'
+import { open, readFile, unlink } from 'fs/promises'
+import { createDecipheriv } from 'crypto'
 import path from 'path'
 
-import { EncryptedJsonStore } from './EncryptedJsonStore'
 import { VaultCrypto } from './VaultCrypto'
 import { VaultPaths } from './VaultPaths'
-import { UploadEvents } from './UploadEvents'
 
 const ALGO = 'aes-256-gcm'
 
 export class VaultFileStore {
   constructor(
-    private jsonStore: EncryptedJsonStore,
     private crypto: VaultCrypto,
     private paths: VaultPaths,
-    private events: UploadEvents,
   ) {}
-
-  async add(vault: VaultEntry, key: Buffer, filepath: string): Promise<string> {
-    const fileId = randomUUID()
-
-    const statResult = await stat(filepath)
-    const total = statResult.size
-
-    this.events.emit('started', {
-      uploadId: fileId,
-      filename: path.basename(filepath),
-      total,
-    })
-
-    const { iv, cipher } = this.crypto.createEncryptionStream(key)
-    const encryptedFilepath = this.paths.data(vault.location, fileId)
-
-    const input = createReadStream(filepath)
-    const output = createWriteStream(encryptedFilepath)
-
-    let transferred = 0
-
-    input.on('data', (chunk: Buffer | string) => {
-      transferred += chunk.length
-
-      this.events.emit('progress', {
-        uploadId: fileId,
-        transferred,
-        total,
-        percent: Math.round((transferred / total) * 100),
-      })
-    })
-
-    output.write(iv)
-    await pipeline(input, cipher, output)
-    await appendFile(encryptedFilepath, cipher.getAuthTag())
-
-    const mimeType = mime.lookup(filepath)
-    const metadata: VaultFileMeta = {
-      fileId,
-      original: {
-        name: path.basename(filepath),
-        ext: path.extname(filepath),
-        mime:
-          typeof mimeType === 'string' ? mimeType : 'application/octet-stream',
-      },
-    }
-
-    const metaPath = this.paths.meta(vault.location, fileId)
-    await this.jsonStore.write(metaPath, metadata, key)
-
-    this.events.emit('finished', {
-      uploadId: fileId,
-      fileId,
-    })
-
-    return fileId
-  }
 
   async read(vault: VaultEntry, key: Buffer, fileId: string): Promise<Buffer> {
     const encryptedFile = await readFile(
@@ -147,10 +79,10 @@ export class VaultFileStore {
     key: Buffer,
     fileId: string,
   ): Promise<VaultFileMeta> {
-    return await this.jsonStore.read(
+    const encryptedFile = await readFile(
       this.paths.meta(vault.location, fileId),
-      key,
     )
+    return JSON.parse(this.crypto.decrypt(encryptedFile, key).toString('utf-8'))
   }
 
   async delete(vault: VaultEntry, fileId: string): Promise<void> {
