e77cad3c9eebd493cbf0244154a4a69b369c2f61
Twaish <axiomeverything@gmail.com>
2026-06-04 22:47:50 +0200

    Remove unused Toggle, ToggleGroup, TextArea, ButtonRippler, and Badge component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,6 @@ import { Slot as SlotPrimitive } from 'radix-ui'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/utils/tailwind'
-import { Rippler } from './button-rippler'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center rounded-sm whitespace-nowrap text-xs font-medium ring-offset-background cursor-pointer transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 relative',
@@ -53,7 +52,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       >
         {children}
-        <Rippler buttonRef={localRef} />
       </Comp>
     )
   },
