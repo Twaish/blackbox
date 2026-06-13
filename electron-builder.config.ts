791084cf0497feff19dec32382201ce57c64b8af
Twaish <axiomeverything@gmail.com>
2026-06-13 20:05:42 +0200

    Fix unused drizzle dir and add publish section and modify artifactName in electron-builder config

--- electron-builder.config.ts
+++ electron-builder.config.ts
@@ -1,6 +1,9 @@
 import type { Configuration } from 'electron-builder'
 import app from './package.json'
 
+const artifactNameTemplate = (platform: string) =>
+  `\${productName}-${platform}-Setup-\${version}-\${arch}.\${ext}`
+
 const config: Configuration = {
   appId: app.appId,
   productName: app.productName,
@@ -8,13 +11,31 @@ const config: Configuration = {
     output: 'release',
     buildResources: 'assets',
   },
-  files: ['out/**/*', 'drizzle/**/*', 'package.json'],
+  files: ['out/**/*', 'package.json'],
   asar: true,
   asarUnpack: ['**/*.node'],
-  win: { target: 'nsis', icon: 'assets/icon.ico' },
-  mac: { target: 'dmg', icon: 'assets/icon.icns' },
-  linux: { target: ['AppImage', 'deb'], icon: 'assets/icon.png' },
-  publish: null,
+  win: {
+    target: 'nsis',
+    icon: 'assets/icon.ico',
+    artifactName: artifactNameTemplate('Windows'),
+  },
+  mac: {
+    target: 'dmg',
+    icon: 'assets/icon.icns',
+    artifactName: artifactNameTemplate('Mac'),
+  },
+  linux: {
+    target: ['AppImage', 'deb'],
+    icon: 'assets/icon.png',
+    artifactName: artifactNameTemplate('Linux'),
+  },
+  publish: [
+    {
+      provider: 'github',
+      owner: app.author,
+      repo: 'blackbox',
+    },
+  ],
 }
 
 export default config
